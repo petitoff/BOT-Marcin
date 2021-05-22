f2383f035dc024997ec51a5dab676b6c33dd03cf
Błażej Domagała <playgame12391@gmail.com>
2021-05-22 16:47:05 +0200

    poziom na którym działa wysyłanie wiadomości przed użytkownika i bota. Wygląd również jest poprawny (CSS działa)

--- app.py
+++ app.py
@@ -8,6 +8,13 @@ def index():
     return render_template('index.html')
 
 
+@app.route("/get")
+def get_bot_response():
+    user_text = request.args.get('msg')
+    print(user_text)
+    user_text2 = user_text
+    return user_text2
+
+
 if __name__ == '__main__':
-    app.debug = True
     app.run()
